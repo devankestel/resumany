5a0ec24a8e168f36e4fbca4f02b6ccca09594173
Devan Kestel <devan.kestel@gmail.com>
2015-07-23 14:15:15 -0400

    Partial structure completed.
    Actual partials still need to be put in.

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,5 +1,8 @@
 class ResumesController < ApplicationController
   def show
     @resume = Resume.find(params[:id])
+    @links = @resume.links.all
+    @experiences = @resume.experiences.all
+    @demonstrations = @experiences.demonstrations.all
   end
 end

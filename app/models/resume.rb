aabb5db9d4951e430d3bdd05f8b5c8f63b7bc820
Devan Kestel <devan.kestel@gmail.com>
2015-07-26 21:13:20 -0400

    Text resume with experiences is writeable.
    Still need to order experiences by category, have file name
    auto-generated, and other small details. Basic formatting is in place.

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -15,8 +15,10 @@ class Resume < ActiveRecord::Base
     # /Users/devankestel1/Documents/resumany/app/assets/txt/name.txt
     File.open("/Users/devankestel1/Documents/resumany/app/assets/txt/name.txt", 'w') do |file| 
       header_txt(file)
+      experiences_txt(file, route) 
     end
   end
+
   def header_txt(file)
     file.puts name
     file.puts ""
@@ -25,6 +27,23 @@ class Resume < ActiveRecord::Base
     file.puts ""
     self.links.each do |link|
       file.puts "  * #{link.title}: #{link.url}"
+    end
+    file.puts ""
+  end
+
+  def experiences_txt(file, route)
+    self.experiences.each do |experience|
+      demos = false
+      file.puts experience.organization
+      file.puts experience.title
+      file.puts experience.daterange
+      file.puts experience.description if experience.description
+      file.puts ""
+      experience.selected_demos(route).each do |demo|
+        file.puts "  * #{demo.description}"
+        demos = true
+      end
+      file.puts "" if demos
     end
   end
 end

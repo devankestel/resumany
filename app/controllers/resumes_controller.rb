795fed61a141aeebc16fb45bf3e09e8aa9644866
Devan Kestel <devan.kestel@gmail.com>
2015-08-01 17:37:23 -0400

    set_experiences method in resumes controller allows for custom ordering.
    Still need to work on how category names will display in Views.

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -33,11 +33,21 @@ class ResumesController < ApplicationController
     @resume = Resume.find(1)
     @route = params[:id]
     @links = @resume.links.all
-    @educational_experiences = @resume.experiences.where(category: "education")
-    @paid_experiences = @resume.experiences.where(category: "paid")
+    set_experiences(@resume, ["education", "paid"])
+    #@educational_experiences = @resume.experiences.where(category: "education")
+    #@paid_experiences = @resume.experiences.where(category: "paid")
     #@resume.write_txt(@route)
     #@resume.write_docx(@route)
     #@resume.write_pdf(@route)
+  end
+
+  def set_experiences(resume, ordered_array)
+    @experiences_collections = []
+    ordered_array.each do |category|
+      collection = resume.experiences.where(category: category)
+      @experiences_collections << collection
+    end
+    @experiences_collections
   end
 
 end
